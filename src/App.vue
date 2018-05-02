18b5d40edcde60a5a1767f5833718e7a7538cc38
Samuel Ojumah <samuelcesc@gmail.com>
2018-05-02 16:21:45 +0100

    Initial Commit - Complete PWA app based on tutorial

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <div class="wrapper">
+        <div class="books">
+          <book v-for="list in lists" :key="list.imageUrl" :list="list"></book>
+        </div>
+      </div>
+      <!-- <img src="./assets/logo.png" alt="Vue.js PWA"> -->
+      <!-- <router-view></router-view> -->
+    </main>
+  </div>
+</template>
+
+<script>
+    import data from './db.json'
+    import Book from './components/Book'
+
+    export default {
+      name: 'app',
+      data () {
+        return {
+          lists: []
+        }
+      },
+      created () {
+        this.lists = data
+      },
+      components: {
+        Book
+      }
+    }
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  main {
+    text-align: center;
+    margin-top: 40px;
+  }
+
+  header {
+    margin: 0;
+    height: 56px;
+    padding: 0 16px 0 24px;
+    background-color: #35495E;
+    color: #ffffff;
+  }
+
+  header span {
+    display: block;
+    position: relative;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: .02em;
+    font-weight: 400;
+    box-sizing: border-box;
+    padding-top: 16px;
+  }
+  .books {
+    column-count: 1;
+    column-gap: 1em;
+  }
+    @media only screen and (min-width: 500px) {
+      .books {
+        column-count: 2;
+      }
+    }
+    @media only screen and (min-width: 700px) {
+      .books {
+        column-count: 3;
+      }
+    }
+    @media only screen and (min-width: 900px) {
+      .books {
+        column-count: 4;
+      }
+    }
+    @media only screen and (min-width: 1100px) {
+      .books {
+        column-count: 5;
+      }
+    }
+</style>
